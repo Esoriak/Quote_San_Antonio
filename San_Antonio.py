7571db859498fbfc0d7506bde658abf347e89e2b
Esoriak <somahaut.dev@gmail.com>
2020-05-04 04:22:12 +0200

    add first script who get a random quote and random characters in list and print it with string format

--- San_Antonio.py
+++ San_Antonio.py
@@ -1,4 +1,6 @@
 # -*- coding: utf8 -*-
+import random
+
 
 quotes = [
     "Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !",
@@ -15,23 +17,30 @@ characters = [
     "Kirikou"
 ]
 
-def get_random_quote():
-    # get a random number
-    # get a quote from an array
-    # show the quote in the interpreter
-    pass
 
 def get_random_item_in(my_list):
     # TODO: get a random number
-    item = my_list[0] # get a quote from a list
-    print(item) # show the quote in the interpreter
-    return "program is over" # returned value
+    rand_numb = random.randint(0, len(my_list) -1)
+    item = my_list[rand_numb] # get a quote from a list
+    #print(item) # show the quote in the interpreter
+    return item # returned value
+
+
+def capitalize(words):
+    for word in words:
+            word.capitalize()
+
+
+
+def message(character, quote):
+    capitalize(character)
+    capitalize(quote)
+    return "{} a dit : {}".format(character, quote)
 
 user_answer = input('Tapez entrée pour découvrir une autre citation ou B pour quitter le programme.')
 
-for character in characters :
-    character.capitalize()
+
 
 while user_answer != "B" :
-    print(get_random_item_in(quotes))
+    print(message(get_random_item_in(characters), get_random_item_in(quotes)))
     user_answer = input('Tapez entrée pour découvrir une autre citation ou B pour quitter le programme.')
